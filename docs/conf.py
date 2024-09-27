7d6c58f012644cd86b7a9692dcd84980d13ea0a5
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-09-27 13:04:16 -0400

    Initial repository setup

---
+++ docs/conf.py
@@ -0,0 +1,71 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../dcnodatg/'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Data Center Network on Demand'
+copyright = '2024, Mencken Davidson'
+author = 'Mencken Davidson'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.napoleon',
+    'myst_parser',
+    'sphinx.ext.autodoc',  # Core Sphinx library for auto html doc generation from docstrings
+    'sphinx.ext.autosummary',  # Create neat summary tables for modules/classes/methods etc
+    'sphinx.ext.intersphinx',  # Link to other project's documentation (see mapping below)
+    'sphinx.ext.viewcode',  # Add a link to the Python source code for classes, functions etc.
+    'sphinx_autodoc_typehints', # Automatically document param types (less noise in class signature)
+    ]
+
+source_suffix = [".rst",  ".md"]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+autosummary_imported_members = True
+autoclass_content = "both"  # Add __init__ doc (ie. params) to class summaries
+html_show_sourcelink = False  # Remove 'view source code' from top of page (for html, not python)
+autodoc_inherit_docstrings = False  # If no docstring, inherit from base class
+set_type_checking_flag = True  # Enable 'expensive' imports for sphinx_autodoc_typehints
+autodoc_typehints = "description"
+autodoc_class_signature = "separated"
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'python_docs_theme'
+
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+#if not on_rtd:  # only import and set the theme if we're building docs locally
+#    import sphinx_rtd_theme
+#    html_theme = "sphinx_rtd_theme"
+#    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_css_files = ["readthedocs-custom.css"] # Override some CSS settings
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+
+html_static_path = ['_static']
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+.. toctree::
+   :hidden:
+
+   Home <self>
+   README
+   Code Documentation <_autosummary/dcnodatg>
+
+
+Data Center Network On Demand
+=============================
+
+A python utility for grabbing run-state details from a list of Arista switches and converting them to a GNS3 virtual-lab topology using cEOS images.
