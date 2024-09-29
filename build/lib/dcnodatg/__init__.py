5cbb2df2f2f0f3fc9448d12587d0e7b2b0052311
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-09-29 01:04:36 -0400

    more build cleanup

---
+++ build/lib/dcnodatg/__init__.py
@@ -0,0 +1,6 @@
+# dcnodatg/__init__.py
+"""Data Center Network on Demand - Arista to GNS3
+
+This package has only one module/script:
+    - 'dcnodatg': Does the deed
+"""
--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../src/dcnodatg/'))
+sys.path.insert(0, os.path.abspath('../dcnodatg/'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
