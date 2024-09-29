edef7ae3bd8741e205f2214fefef148681a61166
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-09-29 12:09:29 -0400

    build/publish fiddling

--- src/dcnodatg/__init__.py
+++ src/dcnodatg/__init__.py
@@ -5,4 +5,3 @@
 This module/script gathers and parses run-state details from a list of Arista
 network switches.  It then creates a GNS3 virtual-lab project in which the
 interrogated devices are emulated."""
-"""
