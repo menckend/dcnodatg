71064d997a12822bb1e413427029cfd8177ec2ee
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-23 11:31:38 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ extensions = [
     'sphinx.ext.autosummary',  # Create neat summary tables for modules/classes/methods etc
     'sphinx.ext.intersphinx',  # Link to other project's documentation (see mapping below)
     'sphinx.ext.viewcode',  # Add a link to the Python source code for classes, functions etc.
-    'sphinx_autodoc_typehints' # Automatically document param types (less noise in class signature)
+    'sphinx_autodoc_typehints', # Automatically document param types (less noise in class signature)
     'autoapi.extension'
     ]
 
